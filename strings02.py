5e214e6a0a353c6aab2f5ece99475c83b70a968a
karthikeyayadavdasari-45 <karthikeyayadavdasari@gmail.com>
2026-04-26 23:58:33 +0530

    pratice on strings

---
+++ strings02.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+
+if name == "Leo":
+    print("Welcome Boss!")
+elif name == "Karthik":
+    print("Hello Karthik!")
+else:
+    print("Hello Guest!")
+
+city = input("Enter your city: ")
+
+if city == "Mumbai":
+    print("Big city life!")
+else:
+    print("Nice place!")
