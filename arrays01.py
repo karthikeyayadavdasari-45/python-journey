8f36b3daea709c439c72aacec20b3f5ed2e8912e
karthikeyayadavdasari-45 <karthikeyayadavdasari@gmail.com>
2026-04-27 11:38:46 +0530

    Doing programs on arrays

---
+++ arrays01.py
@@ -0,0 +1,21 @@
+arr = [10, 20, 40, 50, 70, 2007]
+print(arr[4])
+print(arr[2])
+print(arr[5])
+
+#print all elements
+arr = [2, 4, 5, 6, 70,8]
+for i in arr:
+    print(i)
+# print sum 
+arr = [1,3,5,6,70,80]
+total = 0
+for i in arr:
+    total += i
+print("sum=", total)    
+# print even
+arr = [5,10,4,8]
+for i in arr:
+    if i%2 == 0:
+        print("even")
+
